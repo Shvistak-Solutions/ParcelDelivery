67c00a6ff381524db0dcbf8e0db383efc0aee99e
ptrkfrsk <ptrqfrsq@gmail.com>
2020-05-23 13:08:36 +0200

    Added logout from main coordinator activity and first backbutton override

--- ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/ui/coordinator/CoordinatorActivity.java
+++ ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/ui/coordinator/CoordinatorActivity.java
@@ -1,13 +1,16 @@
 package com.example.ParcelDelivery.ui.coordinator;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.ParcelDelivery.R;
+import com.example.ParcelDelivery.ui.login.LoginActivity;
 import com.example.ParcelDelivery.ui.parcel.ParcelListActivity;
 import com.example.ParcelDelivery.ui.password_reset.ChangePasswordActivity;
 
@@ -22,8 +25,27 @@ public class CoordinatorActivity extends AppCompatActivity {
 
         userId = getIntent().getIntExtra("userId", 0);
 
-        Button buttonParcelList = (Button) findViewById(R.id.buttonParcelList);
+        // logout
+        Button buttonLogout = findViewById(R.id.buttonLogoutCoord);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CoordinatorActivity.this, LoginActivity.class);
+                Toast.makeText(CoordinatorActivity.this, "Wylogowano", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
 
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                Toast.makeText(CoordinatorActivity.this, "Nope", Toast.LENGTH_SHORT).show();
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
+
+        // list of packages
+        Button buttonParcelList = (Button) findViewById(R.id.buttonParcelList);
         buttonParcelList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
