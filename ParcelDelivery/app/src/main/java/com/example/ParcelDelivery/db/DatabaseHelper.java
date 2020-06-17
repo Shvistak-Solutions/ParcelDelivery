5b31732c04f0d0a9a6d435e2146007b23bea0fbc
komoniewskiszczepan <komoniewskiszczepan@gmail.com>
2020-06-17 17:17:17 +0200

    Profile picture - step one ( I hope out of one)

--- ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/db/DatabaseHelper.java
+++ ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/db/DatabaseHelper.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -26,6 +29,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private String TAB_SCHEDULE = "Grafik";
     private String TAB_CLIENTS = "Klienci";
     private String TAB_PACKAGES = "Paczki";
+    private String TAB_AVATAR = "Avatar";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, databaseVersion);
@@ -41,6 +45,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         database.execSQL("CREATE TABLE "+TAB_AVAILABILITY+"(id INTEGER PRIMARY KEY AUTOINCREMENT, id_prac INTEGER, data DATE , godzina_rozpoczecia DATETIME, godzina_zakonczenia DATETIME, FOREIGN KEY(id_prac) REFERENCES Pracownicy(id), UNIQUE(id_prac, data));");
         database.execSQL("CREATE TABLE "+TAB_CLIENTS+"(id INTEGER PRIMARY KEY AUTOINCREMENT, imie VARCHAR, nazwisko VARCHAR, adres VARCHAR, kod_pocztowy VARCHAR);");
         database.execSQL("CREATE TABLE "+TAB_PACKAGES+"(id INTEGER PRIMARY KEY AUTOINCREMENT, id_kuriera INTEGER, status INTEGER, id_nadawcy INTEGER, id_odbiorcy INTEGER, FOREIGN KEY(id_kuriera) REFERENCES Pracownicy(id), FOREIGN KEY(id_odbiorcy) REFERENCES Klienci(id), FOREIGN KEY(id_nadawcy) REFERENCES Klienci(id));");
+        database.execSQL("CREATE TABLE "+TAB_AVATAR+"(id INTEGER PRIMARY KEY, image VARCHAR);");
         update = true;
     }
 
@@ -53,6 +58,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         database.execSQL("DROP TABLE IF EXISTS "+TAB_ACCOUNT);
         database.execSQL("DROP TABLE IF EXISTS "+TAB_WORKERS);
         database.execSQL("DROP TABLE IF EXISTS "+TAB_CLIENTS);
+        database.execSQL("DROP TABLE IF EXISTS "+TAB_AVATAR);
         onCreate(database);
         update = true;
     }
@@ -736,6 +742,94 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             res.append(a);
         return res.toString();
     }
+
+    //------------------------- AVATAR -----------------------------
+    public int doesUserHasAvatar( int userId)
+    {
+        SQLiteDatabase  db = this.getReadableDatabase();
+        String query = "SELECT id from Avatar where id = "+userId;
+        Cursor cursor = db.rawQuery(query, null);
+        int result = cursor.getCount();
+        return result;
+    }
+
+    private static Bitmap stringToBitmap(String encodedString) {
+
+        byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+    }
+
+    public void updateAvatar(int usrID, String image){
+        String userIDstring = String.valueOf(usrID);
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT id from Avatar where id = "+usrID;
+        Cursor cursor = db.rawQuery(query,null);
+        ContentValues values = new ContentValues();
+        int tmp = cursor.getCount();
+        long tmp2 =0 ;
+
+
+        if( tmp == 0 )
+        {
+            values.put("id",usrID);
+            values.put("image",image);
+            tmp2 = db.insert(TAB_AVATAR,null,values);
+        }
+
+
+
+        else {
+            cursor.moveToFirst();
+            do{
+
+                tmp = cursor.getInt(0);
+                if (tmp == usrID){
+                    values.put("image",image);
+                    db.update(TAB_AVATAR,values,"id = ?",new String[] {userIDstring});
+                    db.close();
+                    return;
+                }
+            }while (cursor.moveToNext());
+
+            values.put("id",usrID);
+            values.put("image",image);
+            db.insert(TAB_AVATAR,null,values);
+
+        }
+
+
+
+        cursor.close();
+        db.close();
+    }
+
+
+    public Bitmap getAvatarAsBitmap(int userId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Bitmap bitmap;
+        String avatarString;
+        String query = "SELECT image from Avatar where id = "+userId;
+        Cursor cursor = db.rawQuery(query,null);
+        int isPicture = cursor.getCount();
+        if( isPicture == 0){
+            avatarString = "404";
+            int i = 0 ;
+        }
+        else{
+            cursor.moveToFirst();
+            avatarString = cursor.getString(0);
+        }
+        bitmap = stringToBitmap(avatarString);
+        return bitmap;
+    }
+
+    public void deleteProfilePicture( int userId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TAB_AVATAR+ " WHERE id ="+userId);
+        db.close();
+    }
+
+
 
 
 }
--- ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/ui/storekeeper/StorekeeperActivity.java
+++ ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/ui/storekeeper/StorekeeperActivity.java
@@ -1,9 +1,11 @@
 package com.example.ParcelDelivery.ui.storekeeper;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ParcelDelivery.R;
 import com.example.ParcelDelivery.db.DatabaseHelper;
+import com.example.ParcelDelivery.ui.avatar.AvatarActivity;
 import com.example.ParcelDelivery.ui.parcel.ParcelListActivity;
 import com.example.ParcelDelivery.ui.login.LoginActivity;
 
@@ -56,6 +59,31 @@ public class StorekeeperActivity extends AppCompatActivity {
             public void onClick(View v) {
                 intent = new Intent(StorekeeperActivity.this, ParcelListActivity.class);
                 intent.putExtra("userId", userId);
+                startActivity(intent);
+            }
+        });
+
+
+        //---------------- AVATAR -----------------------------
+        ImageView mainAvatarView = (ImageView)findViewById(R.id.ID_MAIN_AVATAR_VIEW);
+        Bitmap bitmap;
+        DatabaseHelper dbH = new DatabaseHelper(this);
+        int avatarExists = dbH.doesUserHasAvatar(userId);
+        if( avatarExists == 0){
+            mainAvatarView.setImageResource(R.drawable.avatar0);
+        }
+        else{
+            bitmap = dbH.getAvatarAsBitmap(userId);
+            mainAvatarView.setImageBitmap(bitmap);
+        }
+
+        Button avatar = (Button) findViewById(R.id.ID_GO_TO_AVATAR);
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StorekeeperActivity.this, AvatarActivity.class);
+                intent.putExtra("userId", userId);
+                intent.putExtra("class","com.example.ParcelDelivery.ui.storekeeper.StorekeeperActivity");
                 startActivity(intent);
             }
         });
