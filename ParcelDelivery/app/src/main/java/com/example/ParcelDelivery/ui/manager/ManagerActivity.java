f95033890769ad947f9ed2ab5eb397fc02353fec
ptrkfrsk <ptrqfrsq@gmail.com>
2020-07-10 14:41:54 +0200

    Manger avatar readded to main activity

--- ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/ui/manager/ManagerActivity.java
+++ ParcelDelivery/app/src/main/java/com/example/ParcelDelivery/ui/manager/ManagerActivity.java
@@ -4,11 +4,16 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.ParcelDelivery.R;
+import com.example.ParcelDelivery.db.DatabaseHelper;
+import com.example.ParcelDelivery.ui.avatar.AvatarActivity;
 import com.example.ParcelDelivery.ui.login.LoginActivity;
 import com.example.ParcelDelivery.ui.user.UserDetailsActivity;
 import com.example.ParcelDelivery.ui.user.UserListActivity;
@@ -79,18 +84,18 @@ public class ManagerActivity extends AppCompatActivity {
 
 
         //---------------- AVATAR -----------------------------
-//        ImageView mainAvatarView = (ImageView)findViewById(R.id.ID_MAIN_AVATAR_VIEW);
-//        Bitmap bitmap;
-//        DatabaseHelper dbH = new DatabaseHelper(this);
-//        int avatarExists = dbH.doesUserHasAvatar(userId);
-//        if( avatarExists == 0){
-//            mainAvatarView.setImageResource(R.drawable.avatar0);
-//        }
-//        else{
-//            bitmap = dbH.getAvatarAsBitmap(userId);
-//            mainAvatarView.setImageBitmap(bitmap);
-//        }
-//
+        ImageView mainAvatarView = (ImageView)findViewById(R.id.ID_MAIN_AVATAR_VIEW);
+        Bitmap bitmap;
+        DatabaseHelper dbH = new DatabaseHelper(this);
+        int avatarExists = dbH.doesUserHasAvatar(userId);
+        if( avatarExists == 0){
+            mainAvatarView.setImageResource(R.drawable.avatar0);
+        }
+        else{
+            bitmap = dbH.getAvatarAsBitmap(userId);
+            mainAvatarView.setImageBitmap(bitmap);
+        }
+
 //        Button avatar = (Button) findViewById(R.id.ID_GO_TO_AVATAR);
 //        avatar.setOnClickListener(new View.OnClickListener() {
 //            @Override
